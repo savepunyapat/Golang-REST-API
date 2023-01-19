dd479f43faa5c71b472724729c61f2c2aed1abc9
savepunyapat <punyapat1412@gmail.com>
2023-01-19 22:29:22 +0700

    update

--- main.go
+++ main.go
@@ -1,17 +1,30 @@
 package main
 
 import (
-	"net/http"
+	"fmt"
+	"log"
 
 	"github.com/gin-gonic/gin"
+	"github.com/joho/godotenv"
 )
 
+func init() {
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error Loading .env file")
+	}
+}
+
 func main() {
+	fmt.Println("JongInwZa")
+
 	r := gin.Default()
+
 	r.GET("/ping", func(c *gin.Context) {
-		c.JSON(http.StatusOK, gin.H{
+		c.JSON(200, gin.H{
 			"message": "pong",
 		})
 	})
-	r.Run() // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
+
+	r.Run()
 }
